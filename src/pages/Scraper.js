fb8f9b7daf96b62273c2100e4039fe0e2f62b1e8
Albert <albert@hotprintco.com>
2026-05-13 06:53:23 +0800

    update extension ID

--- src/pages/Scraper.js
+++ src/pages/Scraper.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
-const EXTENSION_ID = 'fdmnjnbkbknbphichknjepbmglmbckgm';
+const EXTENSION_ID = 'ikegichhgohflbleliekfakapchldaop';
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const NAV_ITEMS = [
